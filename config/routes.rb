c9c6cbd57f16d2c19e5b5f3d2b6af91c524789d6
coraliehaller <haller.coralie.15@gmail.com>
2024-02-22 16:41:25 +0100

    Watch List back end

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,23 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # get "/lists", to: "lists#index"
+  # get "/lists/:id", to:"lists#show"
+
+  # get "/lists/new", to:"lists#new"
+  # post "/lists", to: "lists#create"
+
+  # get "lists/42/bookmarks/new"
+  # post "lists/42/bookmarks"
+
+  # delete "lists/:id/bookmarks", to: "bookmark#destroy"
+
+  delete "restaurants/:id", to: "restaurants#destroy"
+
+
+
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
