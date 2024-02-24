acf8f9f13fe60677f1805bd9ecbe3074714bf866
coraliehaller <haller.coralie.15@gmail.com>
2024-02-24 12:08:24 +0100

    watch list fin vendredi

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -13,12 +13,16 @@ class ListsController < ApplicationController
   end
 
   def create
-    @list = List.new(list_params)
-    @list.save
+    @list = List.create(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
 private
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :photo)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,5 @@ puts List.count
 puts "Lists are destroyed"
 List.destroy_all
 puts "Here are my new lists"
-List.create!(name: "Korean movies")
-List.create!(name: "SF movies")
 puts List.count
+
